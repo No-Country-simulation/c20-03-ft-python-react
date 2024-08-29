47e12e2ec36c7a4f22046562d91c8bbf2bec5f01
root <root@docker01.avillalba.com.ar>
2024-08-29 00:51:29 -0300

    fixv2

--- backend/init.sh
+++ backend/init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+# Instala los paquetes de Python y actualiza los existentes según requirements.txt
+pip install --upgrade -r requirements.txt
+
+# Ejecuta las migraciones de la base de datos
 python manage.py makemigrations
 python manage.py migrate
 
+# Inicia el servidor de desarrollo de Django
 exec python manage.py runserver 0.0.0.0:8000
-
