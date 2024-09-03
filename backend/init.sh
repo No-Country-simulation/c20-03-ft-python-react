a2ef73f8f28451c0e7132f6645417d703aa0283f
Ubuntu <lunix@docker01.avillalba.com.ar>
2024-09-03 00:31:59 -0300

    add endpoints register & lists users

--- backend/init.sh
+++ backend/init.sh
@@ -7,5 +7,12 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 
+
+# Crear superusuario
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+User.objects.create_superuser('avillalba', 'avillalba96@outlook.com', 'SecurePass123')
+EOF
+
 # Inicia el servidor de desarrollo de Django
 exec python manage.py runserver 0.0.0.0:8000
--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
-    path('', include('postgresql_app.urls')),
     path('admin/', admin.site.urls),
+    path('', include('postgresql_app.urls')),
 ]
+
