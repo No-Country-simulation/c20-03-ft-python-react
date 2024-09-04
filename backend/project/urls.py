7942b4fe6b89e70bf6f3dc2b69e460ab7ff5f287
avillalba96 <avillalba96@outlook.com>
2024-09-04 00:05:41 -0300

    add swagger fix v2

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -3,24 +3,34 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.utils.deprecation import MiddlewareMixin
 
-# Define the schema view with custom security definitions
-schema_view = get_schema_view(
-    openapi.Info(
-        title="eCommerce NoCountry",
-        default_version='v1',
-        description="Swagger Documentation",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="avillalba96@outlook.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,  # Make Swagger public
-    permission_classes=[permissions.AllowAny],  # Allow any access to Swagger
-)
+# Middleware para ajustar el esquema y host según la solicitud
+class SchemaHostMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        if hasattr(request, 'scheme') and hasattr(request, 'get_host'):
+            request.schema = request.scheme
+            request.host = request.get_host()
+
+# Define el esquema con configuración dinámica
+def get_schema_view_func():
+    return get_schema_view(
+        openapi.Info(
+            title="eCommerce NoCountry",
+            default_version='v1',
+            description="Swagger Documentation",
+            terms_of_service="https://www.google.com/policies/terms/",
+            contact=openapi.Contact(email="avillalba96@outlook.com"),
+            license=openapi.License(name="BSD License"),
+        ),
+        public=True,
+        permission_classes=[permissions.AllowAny],
+        url=None,  # Usa el esquema actual de la solicitud
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('postgresql_app.urls')),  # Includes URLs from postgresql_app
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include('postgresql_app.urls')),
+    path('swagger/', get_schema_view_func().with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', get_schema_view_func().with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
