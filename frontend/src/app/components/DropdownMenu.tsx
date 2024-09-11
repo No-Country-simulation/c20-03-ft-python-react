0ad02f16dea491b969afdacfbf7ccfda565e3a55
Alejo <acolazurda@gmail.com>
2024-09-10 23:43:06 -0300

    cambios en el Navbar de prueba.

--- frontend/src/app/components/DropdownMenu.tsx
+++ frontend/src/app/components/DropdownMenu.tsx
@@ -8,8 +8,8 @@ const DropdownMenu: React.FC<DropdownMenuProps> = ({ category }) => {
   const items = getDropdownItems(category); // Función ficticia que retorna los elementos por categoría
 
   return (
-    <div className="absolute left-0 top-full mt-2 bg-white shadow-lg py-4 px-6 z-50 w-64">
-      <ul className="space-y-2">
+    <div className="absolute  left-0 top-full mt-2 bg-white shadow-lg py-4 px-6 z-50 insert-0">
+      <ul className="space-y-2 w-screen">
         {items.map((item) => (
           <li key={item} className="hover:bg-gray-100">
             <Link href={`/${category.toLowerCase()}/${item.toLowerCase()}`} className="block px-4 py-2 text-black hover:text-gray-700">
--- frontend/src/app/components/NavLink.tsx
+++ frontend/src/app/components/NavLink.tsx
@@ -12,21 +12,22 @@ const NavLink: React.FC<NavLinkProps> = ({ title }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div
-      className="relative"
+    <div className="relative">
+      <div
+      
       onMouseEnter={() => setIsOpen(true)}
-      onMouseLeave={() => setIsOpen(false)}
-    >
-      <Link href={`/${title.toLowerCase()}`}>
-        {/* Aquí eliminamos la etiqueta <a> */}
-        <span className="text-lg font-medium hover:text-gray-700">{title}</span>
-      </Link>
+      // onMouseLeave={() => setIsOpen(false)}
+      >
+        <Link href={`/${title.toLowerCase()}`}>
+          {/* Aquí eliminamos la etiqueta <a> */}
+          <span className="text-lg font-medium hover:text-gray-700">{title}</span>
+        </Link>
 
-      {isOpen && (
-        <div className="absolute left-0 top-full mt-2 bg-white shadow-lg py-4 px-6 z-50">
+      
+      </div>
+          {isOpen && (
           <DropdownMenu category={title} />
-        </div>
-      )}
+      )} 
     </div>
   );
 };
